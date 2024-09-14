8cb0db39924488c5133a0bf3c6436dbf245efa6b
IlinArtem78 <artem.ilin1994@gmail.com>
2024-09-14 14:24:43 +0300

    NUnit и XUnit

---
+++ Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_p1
+{
+    public class Calculator
+    {
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+
+    }
+
+}
